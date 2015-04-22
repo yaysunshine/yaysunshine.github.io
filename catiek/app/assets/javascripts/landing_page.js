a0236da64fd6ea8d8389924fb58ca58453d3c80a
yaysunshine <merida1592@gmail.com>
2015-04-21 18:44:54 -0700

    adding css

--- catiek/app/assets/javascripts/landing_page.js
+++ catiek/app/assets/javascripts/landing_page.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
 
+  // $('#shake').hover(function(){
+  //     if(!$(this).hasClass('animated')){
+  //         $(this).addClass('animated');
+  //         $(this).stop().effect('shake', {distance:3}, 200);
+  //     }
+  // }, function(){
+  //     $(this).removeClass('animated');
+  // });
+
+
+  // function periodical() {
+  //   $('#shaker').effect('shake', { times:3 }, 200);
+  // }
+  // $(document).ready(function() {
+  //   $('#shaker').hide().fadeIn(600);
+  //   var shake = setInterval(periodical, 5000);
+
+  //   /* updated: click clear */
+  //   $('#shaker').click(function() {
+  //     clearInterval(shake);
+  //   })
+  // });
+  $( document ).mouseover(function(){
+    $('#shaker').effect( "shake" )
+  });
+  console.log("here");
 });
